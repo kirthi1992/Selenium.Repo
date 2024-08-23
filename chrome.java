b33b1a67bccb1edce7bef681d92a773b775112ce
kirthi1992 <kiruthigaraamasamy@gmail.com>
2024-08-23 22:01:15 +0530

    Add files via upload
    added Day1 Homeassignments

---
+++ chrome.java
@@ -0,0 +1,27 @@
+package HomeAssignmentt.Week1.Day1;
+
+public class chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float version = 91.0f;
+		System.out.println("version =" + version);
+		
+		String developer = "Google";
+		System.out.println("developer =" + developer);
+		
+		boolean isBeta = false;
+		System.out.println("isBeta =" + isBeta);
+		
+		int releaseYear = 2008;
+		System.out.println("releaseYear =" + releaseYear);
+		
+		char shortcutKey ='C';
+		System.out.println("shortcutKey =" + shortcutKey);
+		
+				
+
+	}
+
+}
