6ea5ec0f780721cc8810ffbcd29afd60408d5f1f
kirthi1992 <kiruthigaraamasamy@gmail.com>
2024-08-24 04:02:35 +0530

    Add files via upload
    CheckNumberIsPositive

---
+++ CheckNumberIsPositive.java
@@ -0,0 +1,20 @@
+package HomeAssignment.week1.Day2;
+
+public class CheckNumberIsPositive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int version =10;
+		if (version > 0) {
+			System.out.println("Given Number is Positive.");
+		}
+		else
+		{
+			System.out.println("Given Number is Negative");
+		}
+				
+
+	}
+
+}
